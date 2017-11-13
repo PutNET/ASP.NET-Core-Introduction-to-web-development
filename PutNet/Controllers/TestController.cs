a0c2fd4d04da1f5ca650b6a1ce2e43303af06bf1
Arebis <michal.sibera@windowslive.com>
2017-11-13 21:54:38 +0100

    Added project

---
+++ PutNet/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PutNet.Models;
+
+namespace PutNet.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student student)
+        {
+            ViewBag.Student = student;
+
+            return View();
+        }
+    }
+}
---
+++ PutNet/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PutNet.Models
+{
+    public class Student
+    {
+        [Display(Name = "Indeks")]
+        public string Index { get; set; }
+
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+    }
+}
